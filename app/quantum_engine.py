eadb927d8166f76bf619e07039d053acd097343e
masi628 <massimofornara96@gmail.com>
2025-07-16 21:42:56 +0200

    Update quantum_engine.py

--- app/quantum_engine.py
+++ app/quantum_engine.py
@@ -1,11 +1,19 @@
+
 from qiskit_finance.applications.optimization import PortfolioOptimization
-from qiskit_optimization.algorithms import MinimumEigenOptimizer
-from qiskit.algorithms import QAOA
-from qiskit import Aer
+from qiskit_optimization.problems import QuadraticProgram
+from qiskit_algorithms import MinimumEigenOptimizer
+from qiskit.primitives import Sampler
+from qiskit_algorithms.minimum_eigensolvers import SamplingVQE
+from qiskit.circuit.library import TwoLocal
 
 def optimize_portfolio(mu, sigma, budget):
-    portfolio = PortfolioOptimization(mu, sigma, 0.1, budget)
-    qp = portfolio.to_quadratic_program()
-    qaoa = QAOA(reps=1, quantum_instance=Aer.get_backend("aer_simulator"))
-    result = MinimumEigenOptimizer(qaoa).solve(qp)
-    return result.x.tolist(), result.fval
+    portfolio = PortfolioOptimization(expected_returns=mu, covariances=sigma, budget=budget)
+    qp: QuadraticProgram = portfolio.to_quadratic_program()
+
+    ansatz = TwoLocal(len(mu), "ry", "cz", reps=1)
+    sampler = Sampler()
+    vqe = SamplingVQE(sampler=sampler, ansatz=ansatz)
+    optimizer = MinimumEigenOptimizer(vqe)
+
+    result = optimizer.solve(qp)
+    return result.variables, result.fval
