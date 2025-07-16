b70d3947a14d9878b28a26417e0f0ef08ec4fe59
masi628 <massimofornara96@gmail.com>
2025-07-17 00:16:20 +0200

    Update quantum_derivatives.py

--- app/quantum_derivatives.py
+++ app/quantum_derivatives.py
@@ -1,10 +1,4 @@
-from qiskit_finance.applications.estimation import EuropeanCallExpectedValue
-from qiskit.algorithms import AmplitudeEstimation
-from qiskit import Aer
+def price_european_call(spot_price, strike_price, maturity, risk_free_rate, volatility):
+    from math import exp
+    return max(spot_price - strike_price * exp(-risk_free_rate * maturity), 0)
 
-def price_european_call(S, K, T, r, sigma):
-    backend = Aer.get_backend("aer_simulator")
-    option = EuropeanCallExpectedValue(spot_price=S, strike_price=K, volatility=sigma, interest_rate=r, time_to_maturity=T)
-    ae = AmplitudeEstimation(num_eval_qubits=3, quantum_instance=backend)
-    result = ae.estimate(option)
-    return result.estimation
